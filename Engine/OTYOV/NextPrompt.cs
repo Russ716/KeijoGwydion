cf8f786e6fe0d2e30341f7cc0c3ed72db85271d2
Russ Pelton <russ.pelton@primehealthservices.com>
2023-08-07 16:24:06 -0500

    OTYOV dice roller v1

---
+++ Engine/OTYOV/NextPrompt.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.OTYOV {
+    internal class NextPrompt {
+        private Dictionary<int, List<char>> spacePrompts = new Dictionary<int, List<char>>();
+        private int currentPosition = 1;
+
+        private void btnMoveToNextPrompt_Click(object sender, EventArgs e) {
+            int dTen = BetterRandomNumberGenerator.NumberBetween(1, 10);
+            int dSix = BetterRandomNumberGenerator.NumberBetween(1, 6);
+            int spaces = dTen - dSix;
+            int newPosition = currentPosition + spaces;
+
+            if (!spacePrompts.ContainsKey(currentPosition)) {
+                spacePrompts[currentPosition] = new List<char> { 'a' };
+            } else {
+                spacePrompts[currentPosition].Add(GetNextPromptChar(spacePrompts[currentPosition].Last()));
+            }
+
+            // If the user lands on the same space 3 times in a row, advance them to the next space.
+            if (spacePrompts[currentPosition].Count >= 3) {
+                newPosition = GetNextAvailableSpace(newPosition);
+            }
+
+            // Ensure the user cannot go below space 1
+            if (newPosition < 1) {
+                newPosition = 1;
+            }
+
+            // Update the current position with the new position.
+            currentPosition = newPosition;
+
+            rtbMessages.Text += "You move " + spaces + " spaces " + (spaces >= 0 ? "forward" : "backward") + Environment.NewLine;
+        }
+
+        private char GetNextPromptChar(char lastPromptChar) {
+            if (lastPromptChar == 'z') {
+                return 'a';
+            } else {
+                return (char)(lastPromptChar + 1);
+            }
+        }
+
+        private int GetNextAvailableSpace(int position) {
+            // Find the next available space that has not been visited 3 times.
+            int nextSpace = position + 1;
+            char promptChar = 'a';
+
+            while (spacePrompts.ContainsKey(nextSpace) && spacePrompts[nextSpace].Count >= 3) {
+                nextSpace++;
+            }
+
+            if (spacePrompts.ContainsKey(nextSpace)) {
+                promptChar = GetNextPromptChar(spacePrompts[nextSpace].Last());
+            }
+
+            spacePrompts[nextSpace] = new List<char> { promptChar };
+            return nextSpace;
+        }
+
+    }
+}
